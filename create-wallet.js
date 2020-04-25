51d54f2a31cef0e0be62f6336251d4dd6dff154e
liberumChain <59084371+liberumChain@users.noreply.github.com>
2020-04-25 14:08:19 +0800

    Delete create-wallet.js

--- create-wallet.js
+++
@@ -1,9 +0,0 @@
-/**
- * @description 创建钱包
- * @return { address: '*****',secret: '******'} 
- */
-const Wallet = require('./tool/lbr-account')
-
-var w = Wallet.generate()
-let lbrAccount = { address: w.getAddressString(), secret: w.getPrivateKeyString() };
-console.log(lbrAccount)
